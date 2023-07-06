6cc71442cb0037a65f704c494534ca187b302b00
1f10220217 <s1f102202178@iniad.org>
2023-07-06 14:19:35 +0900

    flake8修正4

--- todo/tests.py
+++ todo/tests.py
@@ -3,15 +3,14 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
-# Create your tests here.
-class SampleTestCase(TestCase):
 
+class SampleTestCase(TestCase):
 
     def test_sample1(self):
         self.assertEqual(1+2, 3)
 
-class TaskModelTestCase(TestCase):
 
+class TaskModelTestCase(TestCase):
 
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
@@ -35,7 +34,7 @@ class TaskModelTestCase(TestCase):
     def test_is_overdue_future(self):
         due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2023, 6, 30, 0, 0, 0))
-        task = Task(title = "task1", due_at = due)
+        task = Task(title="task1", due_at=due)
         task.save()
 
         self.assertFalse(task.is_overdue(current))
@@ -43,20 +42,20 @@ class TaskModelTestCase(TestCase):
     def test_is_overdue_past(self):
         due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2023, 7, 1, 0, 0, 0))
-        task = Task(title="task1", due_at = due)
+        task = Task(title="task1", due_at=due)
         task.save()
 
         self.assertTrue(task.is_overdue(current))
 
     def test_is_overdue_none(self):
         current = timezone.make_aware(datetime(2023, 7, 1, 0, 0, 0))
-        task = Task(title = "task2")
+        task = Task(title="task2")
         task.save()
 
         self.assertFalse(task.is_overdue(current))
 
-class TodoViewTestCase(TestCase):
 
+class TodoViewTestCase(TestCase):
 
     def test_index_get(self):
         client = Client()
@@ -100,4 +99,3 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
-        
