0ba681d279115f196a744d0444bfbd300329f913
1f10220210 <s1f102202101@iniad.org>
2023-07-13 14:57:51 +0900

    削除機能の追加２

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,32 @@ from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
+def index(request):
+
+    if request.method == "POST":
+        task = Task(title=request.POST["title"], due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task.save()
+
+    if request.GET.get('order') == "due":
+        tasks = Task.objects.order_by("due_at")
+    else:
+        tasks = Task.objects.order_by("-posted_at")
+
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'todo/index.html', context)
+
+def detail(request,task_id):
+    try:
+        task=Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404('Task does not exist')
+    context = {
+        'task':task,
+    }
+    return render(request,'todo/detail.html',context)
+
 
 def delete(request, task_id):
     try:
